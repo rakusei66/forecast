9609cbd3a00157eb3231ba80e50115869e79a4ed
Githubに登録したユーザーネーム <Githubemail>
2020-01-12 06:05:59 +0000

    update app.rb

---
+++ app.rb
@@ -0,0 +1,64 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+require 'sinatra/activerecord'
+require './models'
+
+get '/' do
+  @histories = History.all.limit(10).order("created_at desc")
+  @favorites = History.where(favorite: true)
+  erb :form
+end
+
+get '/list' do
+  History.create!(x: params[:x],y: params[:y])
+
+
+  uri = URI("http://express.heartrails.com/api/json")
+  uri.query = URI.encode_www_form({
+    method: "getStations",
+    x: params[:x],
+    y: params[:y]
+  })
+  res = Net::HTTP.get_response(uri)
+  json = JSON.parse(res.body)
+  @stations = json["response"]["station"]
+  erb :list
+  end
+
+get '/api/station' do
+  uri = URI("http://express.heartrails.com/api/json")
+  uri.query = URI.encode_www_form({
+    method: "getStations",
+    line: params[:line],
+    name: params[:name]
+  })
+  res = Net::HTTP.get_response(uri)
+  json = JSON.parse(res.body)
+  if json["response"]["error"]
+    response = {error: "No Station."}
+  else
+    response = {
+    next: json["response"]["station"][0]["next"],
+    prev: json["response"]["station"][0]["prev"]
+  }
+  end
+  json response
+  end
+
+post '/:id/delete' do
+  history = History.find(params[:id])
+  history.delete
+  redirect "/"
+end
+
+post '/:id/update' do
+  history = History.find(params[:id])
+  history.favorite = !history.favorite
+  history.save
+  redirect "/"
+end
