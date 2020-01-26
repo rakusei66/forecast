2cd2854642d701fbef5a71244f5363380cb82289
Githubに登録したユーザーネーム <Githubemail>
2020-01-26 05:48:30 +0000

    fix sqlite versions hoge hoge

--- models.rb
+++ models.rb
@@ -3,9 +3,6 @@ ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
 require 'bundler/setup'
 Bundler.require
 
-if development?
-  ActiveRecord::Base.establish_connection('aqlite3:db/development.db')
-end
 
 class History < ActiveRecord::Base
 end
